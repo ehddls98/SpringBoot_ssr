a84e62cc2c8d362a8d82b97b88eec28799dc048a
ehddls98 <icbku1357@naver.com>
2024-07-17 21:13:54 +0900

    Spring boot 1일차

--- src/main/java/com/study/ssr/controller/NameController.java
+++ src/main/java/com/study/ssr/controller/NameController.java
@@ -1,5 +1,6 @@
 package com.study.ssr.controller;
 
+import com.study.ssr.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,10 +8,11 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class NameController {
 
-    @GetMapping("/name")
+    @GetMapping("/name") //url 주소
     public ModelAndView namePage() {
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("views/name");
+        ModelAndView mav = new ModelAndView(); //ModelAndView 객체 생성
+        mav.setViewName("views/name"); // mav 경로 설정
+        mav.addObject(Student.builder().name("김동인").age(27).build());
         return mav;
     }
 
